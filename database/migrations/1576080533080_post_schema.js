483f0b7610823668136f6752104c53aaa9cdb2f1
lazaroAM <44584192+lazaroAM@users.noreply.github.com>
2019-12-11 19:33:28 -0300

    adonis backend

---
+++ database/migrations/1576080533080_post_schema.js
@@ -0,0 +1,21 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+//cria a tabela
+class PostSchema extends Schema {
+  up () {
+    this.create('posts', (table) => {
+      table.increments()
+      table.string('content').notNullable()
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('posts')
+  }
+}
+
+module.exports = PostSchema
